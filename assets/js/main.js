4a2de69d7dc192de0beb00aac7d9b8987ca67214
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-07-15 19:50:31 +0200

    add little style and logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 console.log("Hello world")
+
+let container = document.getElementById("container")
+let numbers = document.getElementsByClassName("numbers")
+
+console.log(container, numbers)
+
+for (let index = 1; index < 13; index++) {
+    console.log(index)
+
+    container.innerHTML = index
+
+}
