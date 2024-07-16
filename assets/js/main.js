a4588c08d83565c83623bfc59568cdb3a35df9aa
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-07-16 16:29:20 +0200

    edit start function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,48 @@ const markup = `<div class="box"></div>`;
 
 console.log(container);
 
-let myArray = [];
+let myArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+
+let flipArray = []
+let successArray = []
+let errorContainer = document.getElementById("error")
+let errors = 0
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffle(myArray);
+
+function verify(array) {
+    let redElements = document.querySelectorAll(".red")
+    if (array.length == 2) {
+        if (flipArray[0] == flipArray[1]) {
+            console.log("corrispondono")
+            flipArray = []
+        } else {
+            console.log("non corrispondono")
+            errors++
+            console.log(errors)
+            errorContainer.innerText = `Errori: ${errors}`
+            setTimeout(() => {
+                flipArray = []
+                redElements.forEach(redElement => {
+                    redElement.innerHTML = "";
+                    redElement.classList.remove("red");
+                });
+            }, 300)
+
+        }
+
+    }
+}
 
 function start() {
-    for (let index = 1; index <= 12; index++) {
-        myArray.push(index);
+    for (let index = 0; index < myArray.length; index++) {
         container.insertAdjacentHTML("beforeend", markup);
     }
 
@@ -19,12 +56,28 @@ function start() {
 
 
         element.addEventListener("click", function () {
-            if (!element.classList.contains("red")) {
-                element.insertAdjacentText("beforeend", index);
+            if (!element.classList.contains("red") && element.innerHTML === "") {
+                if (myArray[index] == 1) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-dragon"></i>`);
+                } else if (myArray[index] == 2) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-otter"></i>`);
+                } else if (myArray[index] == 3) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-hippo"></i>`);
+                } else if (myArray[index] == 4) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-dog"></i>`);
+                }
+                else if (myArray[index] == 5) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-cat"></i>`);
+                }
+                else if (myArray[index] == 6) {
+                    element.insertAdjacentHTML("beforeend", `<i class="fa-solid fa-frog"></i>`);
+                }
+                flipArray.push(myArray[index])
+
                 element.classList.add("red");
+                verify(flipArray)
             }
         });
-        console.log(element);
     }
 }
 
